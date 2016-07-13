6f728cf8e42638c869a4f40afd46f439fa239bd3
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2016-07-13 17:30:43 +0200

    Start working on markdown version of order

--- server/genorder.py
+++ server/genorder.py
@@ -1,61 +1,51 @@
 # encoding: UTF-8
-"""
+'''
     Pizzaboy
     Automated generation of pizza orders.
 
     Authors: Igor Babuschkin <igor.babuschkin@udo.edu>
              Kevin Dungs <kevin.dungs@udo.edu>
     Version: 1.0 (2013-01-30)
-"""
+'''
 
 from __future__ import print_function
+import jinja2
 
 from datetime import datetime
-from decimal import Decimal
-from os import remove
-from re import findall, MULTILINE
-from subprocess import Popen
+
 
 def cents_to_euros(cents):
     return u'{},{:02d} €'.format(int(cents / 100), cents % 100)
 
+
 def escape_latex(pizzas):
-    return pizzas #pizzas.replace("\\", "\\\\").replace("%", "\\%").replace("&", "\\&")
+    return pizzas  # pizzas.replace('\\', '\\\\').replace('%', '\\%').replace('&', '\\&')
+
 
 def sanitize_tex(text):
-    return text.replace('\\', '').replace('{','\\{').replace('}', '\\}')
+    return text.replace('\\', '').replace('{', '\\{').replace('}', '\\}')
+
 
 def print_order(pizzas, prices):
-
-    coststring = u"Preis (mit Rabatt): {} ({})".format(
+    coststring = u'Preis: {}'.format(
         cents_to_euros(sum(prices)),
         cents_to_euros(int(round(0.9 * sum(prices))))
     )
 
     pizzas = map(sanitize_tex, pizzas)
     pizzas = '\\item ' + escape_latex(' \\ \n \\item '.join(pizzas))
-    identifier = "pizza-{}".format(datetime.now().strftime("%Y-%m-%d"))
-    filename = "/tmp/{}.tex".format(identifier)
+    identifier = 'pizza-{}'.format(datetime.now().strftime('%Y-%m-%d'))
+    filename = '/tmp/{}.tex'.format(identifier)
+
     with open(filename, 'w') as texfile:
         texfile.write(
             TEMPLATE.decode('utf-8')
-            .replace(u"%PIZZA", pizzas)
-            .replace(u"%PRICE", coststring)
-            .encode('utf-8'))
-    p = Popen(
-        "lualatex --interaction=batchmode {}".format(filename),
-        shell=True
-    )
-    ret = p.wait()
-    remove("{}.aux".format(identifier))
-    remove("{}.log".format(identifier))
-    return '{}.pdf'.format(identifier)
-    #if ret == 0:
-    #    Popen(
-    #        "cat {}.pdf | lpr -P Kyocera_Mita_FS-3820N_FS-3820N -o Duplex=DuplexNoTumble".format(identifier),
-    #        shell=True
-    #    )
-    #    print("Druckauftrag an KYOA abgeschickt.")
+            .replace(u'%PIZZA', pizzas)
+            .replace(u'%PRICE', coststring)
+            .encode('utf-8')
+        )
 
-with open('template.tex', 'r') as f:
+    return '{}.pdf'.format(identifier)
+
+with open('template.md', 'r') as f:
     TEMPLATE = f.read()
